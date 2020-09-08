7e2e47924036b1a4d808c8e7313b452419acd00c
Shevchenko Elena <elena.kadru@gmail.com>
2020-09-09 00:10:41 +0300

    added images finder

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './components/App';
+import 'modern-normalize/modern-normalize.css';
+import './index.module.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
